df54bf13f85f1bd0afb0b275631a7cd8e380e36c
Micah Barrett <mbarrett02@risd.edu>
2014-08-10 11:58:13 -0400

    initial push

---
+++ assets/js/src/index.js
@@ -0,0 +1,15 @@
+// write JavaScript here
+
+console.log($('p').text());
+
+$(document).ready(function(){
+	console.log($('h1').text());
+	$('h1').lettering();
+
+});
+
+$('.video').hover(function(){
+	$('h1').addClass("fade-in");
+	$('.video').addClass("colorize");
+
+})
